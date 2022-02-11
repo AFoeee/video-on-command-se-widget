44174d918601c68fcd53a901f1a83b22512faf1c
AFoeee <44651916+AFoeee@users.noreply.github.com>
2022-02-11 20:55:08 +0100

    Update to VideoOnCommand v4
    I used Suerion's variation of the "Video On Command" custom widget as a starting point.
    Then I refactored the whole code and added the following features:
     - Introduced structural conventions in 'Fields'.
     - Comparison modes per media command.
     - Case sensitivity option for said comparison.
     - Support for the "multiple" parameter of media input fields.
     - Updated the animate.css library version.
     - Introduced a debug mode.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # VideoOnCommand
 
 My version of the StreamElements "Video On Command" custom widget, originally written by Benno.  
+The code builds on Suerion's variation of said widget.
 
 ## Description of operation:
 This custom widget allows to associate media (video or audio) with chat commands.  
@@ -8,7 +9,21 @@ It plays when the corresponding command has been detected in chat.
 
 Furthermore, the streamer can limit the access to particular users or user categories.
 
+## Previously added features:
+ - User access level `no restrictions`.
+ - A `blocked users` list.
+ - Cooldown option for each video.
+ - Global cooldown (by Suerion).
+ - A total of 15 predefined audio commands (by Suerion).
+
 ## New features:
- - Added the user access level `no restrictions` (to allow non-VIP users to trigger the widget).
- - Each video got a cool down options to prevent spamming.
- - A "blocked users" list was introduced to ignore specific users.
+ - Refactored the whole code and added two lists to the JavaScript, which hold field prefixes.  
+   Those prefixes coupled with structural conventions in 'Fields', should (hopefully) make it easier to append new media commands (like a Xth video command).
+ - Set the number of predefined commands to 5 for each media type. 
+ - Added a dropdown list for each media command, which defines how a chat message is compared to the specified command string.  
+   This way it is also triggered when the message contains additional content (e.g. *"!lurk till later"* or *"I'm in !lurk"*).
+ - Added a case sensitivity option for the string comparison.
+ - Media input fields with the "multiple" parameter set to true are now supported.  
+   If encountered, a random media is picked from the provided pool.
+ - Updated the [animate.css library](https://github.com/animate-css/animate.css) version (3.7.0 -> 4.1.1).
+ - Added a debug mode, that prints information to the console (F12).
--- videoOnCommand.js
+++ videoOnCommand.js
@@ -1,148 +1,397 @@
-// This widget is a simple modification of the 'Video on command' widget by Benno.
-// The comments that start with 'modified' indicate what I added to the code.
-// Basically there were 3 features added:
-// - the user lvl 'no restrictions' (so that non-vip users can trigger the widget).
-// - cool down options for the videos.
-// - a list where users can be specified that are ignored by the widget.
+/* 
+ * A version of the 'Video On Command' widget originally written by Benno.
+ * This code builds on Suerion's variation of the previously mentioned widget.
+ * 
+ * Special thanks to Benno, Suerion, lx, thefyrewire, SquidCharger and ca11.
+ */
 
-// Thanks alot to Benno for creating the original widget.
 
-// Statement from the original widget:
-// This widget contains content of a widget from @lx which was an inspiration for this widget. 
-// Thank you very much @thefyrewire and @lx for helping me with this widget.
+/* Prefixes defined in 'Fields' are only taken into account if they are also
+ * specified in one of the two lists below. They're prioritized in the same order
+ * as they appear in the list. */
+let videoPrefixes = [
+  "video1", 
+  "video2", 
+  "video3", 
+  "video4", 
+  "video5"
+];
 
-let userOptions = {};
-let channels = [];
-let animationIn = 'bounceIn';
-let animationOut = 'bounceOut';
-let timeIn = 400;
-let timeOut = 400;
-let allowed = true;
+let audioPrefixes = [
+  "audio1", 
+  "audio2", 
+  "audio3", 
+  "audio4", 
+  "audio5"
+];
 
-// modified - logs the timestep of the last successful execution.
-let command1_lastSuccessEpoch = 0;
-let command2_lastSuccessEpoch = 0;
+let allowed = false;                // Blocks the widget if necessary.
+let fieldData = {};                 // Config data from 'Fields'.
+let channelName = "";               // Username of the broadcaster.
+let mediaCommands = [];             // Holds all types of MediaCommand objects.
 
-window.addEventListener('onWidgetLoad', function (obj) {
-    userOptions = obj['detail']['fieldData'];
-    animationIn = userOptions['animationIn'];
-    animationOut = userOptions['animationOut'];
-    timeIn = userOptions['timeIn'];
-    timeOut = userOptions['timeOut'];
-    userOptions['channelName'] = obj['detail']['channel']['username'];
-    userOptions['otherUsers'] = (userOptions['otherUsers'].toLowerCase()).replace(/\s/g, '').split(",");
-    $("#video").hide();
-    allowed = true;
+
+/* Triggers CSS animations by adding animate.css classes. Their effect is 
+ * sustained as long as they're attached to the element. Therefore, they are 
+ * only removed shortly before being replaced by other animate.css classes. */
+function animateCss(node, animationName, duration = 1, prefix = 'animate__') {
+  // animate.css classes do have a prefix (since version 4.0).
+  const envCls = `${prefix}animated`;
+  const animationCls = `${prefix}${animationName}`;
   
-    // modified - prepare the list of ignored users.
-    userOptions['blockedUsers'] = (userOptions['blockedUsers'].toLowerCase()).replace(/\s/g, '').split(",");
-});
+  // Remove all applied animate.css classes.
+  node.className = node.className
+      .split(" ")
+      .filter((cls) => !cls.startsWith(prefix))
+      .join(" ");
+  
+  // Promise resolves when animation has ended.
+  return new Promise((resolve, reject) => {
+    node.addEventListener('animationend', (event) => {
+      event.stopPropagation();
+      resolve('Animation ended');
+    }, {once: true});
+    
+    node.style.setProperty('--animate-duration', `${duration}s`);
+    node.classList.add(envCls, animationCls);       // Starts CSS animation.
+  });
+}
 
-window.addEventListener('onEventReceived', function (obj) {
-    if (obj.detail.listener !== 'message') return;
-    let data = obj.detail.event.data;
-    let message = data['text'].toLowerCase();
-    if (message !== userOptions['command1'] && message !== userOptions['command2'] && message !== userOptions['command3'] && message !== userOptions['command4']) return;
-    console.log("Got it! " + message);
-    let user = data['nick'].toLowerCase();
 
-    //Preparing userState object containing all user flags
-    let userState = {
-        'mod': parseInt(data.tags.mod),
-        'sub': parseInt(data.tags.subscriber),
-        'vip': (data.tags.badges.indexOf("vip") !== -1),
-        'broadcaster': (user === userOptions['channelName'])
-    };
+// Convenience function for debugging mode.
+function log(msg) {
+  if (fieldData.debugMode !== "enabled") return;
+  
+  console.log(msg);
+}
 
-    if (!allowed) return;
 
-    //modified - added permission level 'everyone'.
-    if ((userOptions['managePermissions'] === 'everyone') || (userState.mod && userOptions['managePermissions'] === 'mods') || ((userState.vip || userState.mod) && (userOptions['managePermissions'] == 'vips')) || userState.broadcaster || (userOptions['otherUsers'].indexOf(user) !== -1)) {
-        // modified - only execute if user is not ignored.
-        if ((userOptions['blockedUsers'].indexOf(user) === -1)) {
-            let video = $("#video");
-            let source = $("#source");
-            allowed = false;
-            video[0].pause();
+/* Abstract base class, which defines ...
+ * ... general media attributes.
+ * ... how the command is triggered.
+ * ... how the cooldown mechanism works. 
+ * ... which url is used (if multiple are provided). */
+class MediaCommand {
+  static globalCooldownMillis = {{globalcooldown}} * 1000;
+  static globalCooldownEndEpoch = 0;
+  
+  cooldownMillis;
+  cooldownEndEpoch = 0;
+  
+  commandStr;
+  #url;
+  normalizedVolume;
+  
+  isTriggeredBy;
+  
+  constructor(
+      commandStr, 
+      url, 
+      volumePct, 
+      cooldownSec, 
+      comparisonMode = "strict", 
+      isCaseSensitive) {
+    
+    if (new.target === MediaCommand) {
+      throw TypeError("new of abstract class MediaCommand.");
+    }
+    
+    this.commandStr = commandStr;
+    this.#url = url;
+    this.normalizedVolume = volumePct / 100;
+    this.cooldownMillis = cooldownSec * 1000;
+    
+    // The function that gets selected determines how a match is defined.
+    if (comparisonMode === 'regex') {
+      /* Converting a regex string to lower case is problematic as it might 
+       * change the meaning. Therefore the 'i' flag is used, which indicates
+       * that case should be ignored while attempting a match in a string. */
+      let re = new RegExp(
+          commandStr, 
+          isCaseSensitive ? undefined : 'i');
+      
+      this.isTriggeredBy = (msg) => re.test(msg);
+      
+    } else {
+      /* Case insensitivity is achieved by converting both strings to lower case.
+       * The convertion of the chat message happens through a decorator wrapper. */
+      let cmd = isCaseSensitive ? commandStr : commandStr.toLowerCase();
+      let compFunc;
+      
+      switch (comparisonMode) {
+        case 'strict': 
+            compFunc = (msg) => (msg === cmd);
+            break;
+            
+        case 'firstWord': 
+            compFunc = (msg) => (msg.trimStart().split(' ', 1)[0] === cmd);
+            break;
+            
+        case 'firstWordStartsWith': 
+            compFunc = (msg) => msg.trimStart().startsWith(cmd);
+            break;
+            
+        case 'someWord': 
+            compFunc = (msg) => msg.split(' ').includes(cmd);
+            break;
+            
+        case 'someWordStartsWith': 
+            compFunc = 
+                (msg) => msg.split(' ').some(
+                    (str) => str.startsWith(cmd));
+            break;
+            
+        default: 
+            throw new Error(
+                this.constructor.name + 
+                " constructor encountered an unknown switch value: " + 
+                comparisonMode);
+      }
+      
+      if (isCaseSensitive) {
+        this.isTriggeredBy = compFunc;
+      } else {
+        // Said decorator wrapper, that converts the chat msg to lower case.
+        this.isTriggeredBy = (msg) => compFunc(msg.toLowerCase());
+      }
+    }
+  }
+  
+  get url() {
+    /* If an array of media is provided a random element is picked. (That's the 
+     * case when the field's "multiple" parameter is true.) */
+    if (Array.isArray(this.#url)) {
+      const randomIndex = Math.floor(Math.random() * this.#url.length);
+      return this.#url[randomIndex];
+    }
+    return this.#url;
+  }
+  
+  set url(newUrl) {
+    this.#url = newUrl;
+  }
+  
+  /* Sets a global cooldown and an individual cooldown. To avoid unnecessary 
+   * calculations, each is represented by an epoch time that marks the moment, 
+   * when its respective cooldown has ended. Later code then simply compares 
+   * those results to the current epoch time. */
+  activateCooldown() {
+    let now = Date.now();
+    
+    // Global cooldown.
+    MediaCommand.globalCooldownEndEpoch = 
+        now + MediaCommand.globalCooldownMillis;
+    
+    // Individual cooldown.
+    this.cooldownEndEpoch = now + this.cooldownMillis;
+  }
+  
+  static isOnGlobalCooldown() {
+    return (Date.now() < MediaCommand.globalCooldownEndEpoch);
+  }
+  
+  isOnCooldown() {
+    return (Date.now() < this.cooldownEndEpoch);
+  }
+  
+  // Abstract method.
+  play() {
+    throw new Error("play() wasn't implemented by the child class.");
+  }
+}
 
-            if (message == userOptions['command1']) {
-                // modified - cancel execution if the command is still on cool down.
-                if (!command1_isOnCoolDown()) {
-                    video[0].load();
-                    source.attr('src', '{{video1}}');
-                    video[0].volume = {volumevid1}/100;
-                    play();
-                    command1_lastSuccessEpoch = Date.now();
-                } else {
-                    allowed = true;
-                };
-            } else if (message == userOptions['command2']) {
-                // modified - cancel execution if the command is still on cool down.
-                if (!command2_isOnCoolDown()) {
-                    video[0].load();
-                    source.attr('src', '{{video2}}');
-                    video[0].volume = {volumevid2}/100;
-                    play();
-                    command2_lastSuccessEpoch = Date.now();
-                } else {
-                    allowed = true;
-                };
-            } else if (message == userOptions['command3']) {
-                let sfx = new Audio("{{audio1}}");
-                sfx.volume = userOptions['volumeaudio1'] * .01;
-                sfx.play();
-                sfx.onended = function () {
-                    allowed = true;
-                };
-            } else if (message == userOptions['command4']) {
-                let sfx2 = new Audio("{{audio2}}");
-                sfx.volume = userOptions['volumeaudio2'] * .01;
-                sfx2.play();
-                sfx2.onended = function () {
-                    allowed = true;
-                }    
-            };
-    	};
+
+class VideoCommand extends MediaCommand {
+  static videoElmt = document.getElementById("video");
+  
+  static {
+    // Hide video element immediately (therefore, 0s duration).
+    let hideVideoElmt = 
+        () => animateCss(VideoCommand.videoElmt, "{{animationOut}}", 0);
+    
+    hideVideoElmt();
+    
+    // When a video playback starts, trigger the in animation.
+    VideoCommand.videoElmt.onplay = () => {
+      animateCss(VideoCommand.videoElmt, "{{animationIn}}", {{timeIn}});
     };
     
-    function play() {
-        let video = $("#video");
-        video.addClass(animationIn + ' animated', timeIn)
-            .show(0, timeIn)
-            .removeClass(animationIn)
-            .get(0).play();
-        video.on('ended', function () {
-            video.addClass(animationOut, timeOut)
-                .removeClass(animationOut + " animated", timeOut)
-                .hide(0, timeOut);
-            allowed = true;
-        });
-    }
+    // When an error occurs, unblock the widget.
+    VideoCommand.videoElmt.onerror = () => {
+      hideVideoElmt();
+      allowed = true;
+      
+      throw VideoCommand.videoElmt.error;
+    };
+    
+    // When a video ends, trigger the out animation.
+    VideoCommand.videoElmt.onended = () => {
+      let animateCssPromise = 
+          animateCss(VideoCommand.videoElmt, "{{animationOut}}", {{timeOut}});
+      
+      /* Unblock the widget only after the out animation has finished. Otherwise
+       * there would be a chance that it's interrupted. */
+      animateCssPromise
+          .finally(() => { allowed = true; });
+    };
+  }
+  
+  async play() {
+    VideoCommand.videoElmt.pause();
+    
+    VideoCommand.videoElmt.src = this.url;
+    VideoCommand.videoElmt.volume = this.normalizedVolume;
+    
+    // 'load() will reset the element and rescan the available sources ...'
+    VideoCommand.videoElmt.load();
+    
+    allowed = false;
+    
+    VideoCommand.videoElmt.play();
+    
+    this.activateCooldown();
+  }
+}
 
-    // modified - the command isn't executed as long as this returns true.
-    function command1_isOnCoolDown() {
-        let elapsedMillis = Date.now() - command1_lastSuccessEpoch;
-        let coolDownMillis = {cooldown1}*1000;
-        
-        if (elapsedMillis > coolDownMillis) {
-            return false;
-        } else {
-            console.log("Command1 was executed " + elapsedMillis + " milliseconds ago and is therefore still on cool down. (Total cool down: " + coolDownMillis + " milliseconds.)");
-            return true;
-        };
-    }
 
-    // modified - the command isn't executed as long as this returns true.
-    function command2_isOnCoolDown() {
-      	let elapsedMillis = Date.now() - command2_lastSuccessEpoch;
-        let coolDownMillis = {cooldown2}*1000;
-        
-        if (elapsedMillis > coolDownMillis) {
-            return false;
-        } else {
-            console.log("Command2 was executed " + elapsedMillis + " milliseconds ago and is therefore still on cool down. (Total cool down: " + coolDownMillis + " milliseconds.)");
-            return true;
-        };
+class AudioCommand extends MediaCommand {
+  play() {
+    let sfx = new Audio(this.url);
+    sfx.volume = this.normalizedVolume;
+    
+    allowed = false;
+    
+    // When the audio playback ends, unblock the widget.
+    sfx.onended = () => { 
+      allowed = true;
+    };
+    
+    // When an error occurs, unblock the widget, too.
+    sfx.onerror = () => {
+      allowed = true;
+      throw sfx.error;
+    };
+    
+    sfx.play();
+    
+    this.activateCooldown();
+  }
+}
+
+
+window.addEventListener('onWidgetLoad', function (obj) {
+  fieldData = obj.detail.fieldData;
+  
+  // Convert user lists to arrays.
+  fieldData.otherUsers = fieldData.otherUsers
+      .toLowerCase()
+      .replace(/\s/g, '')
+      .split(",");
+  fieldData.blockedUsers = fieldData.blockedUsers
+      .toLowerCase()
+      .replace(/\s/g, '')
+      .split(",");
+  
+  channelName = obj.detail.channel.username;
+  
+  let isCaseSensitive = (fieldData.caseSensitivity === "enabled");
+  
+  // Initialize MediaCommands and ignore any command without associated URL(s).
+  videoPrefixes.forEach((prefix) => {
+    log(`Start initialization for video prefix '${prefix}'.`);
+    
+    let url = fieldData[`${prefix}_url`];
+    if (url) {
+      mediaCommands.push(new VideoCommand(
+          fieldData[`${prefix}_command`], 
+          url, 
+          fieldData[`${prefix}_volume`], 
+          fieldData[`${prefix}_cooldown`], 
+          fieldData[`${prefix}_comparisonMode`], 
+          isCaseSensitive));
     }
+  });
+  
+  audioPrefixes.forEach((prefix) => {
+    log(`Start initialization for audio prefix '${prefix}'.`);
+    
+    let url = fieldData[`${prefix}_url`];
+    if (url) {
+      mediaCommands.push(new AudioCommand(
+          fieldData[`${prefix}_command`], 
+          url, 
+          fieldData[`${prefix}_volume`], 
+          fieldData[`${prefix}_cooldown`], 
+          fieldData[`${prefix}_comparisonMode`], 
+          isCaseSensitive));
+    }
+  });
+  
+  // Unblock the widget when successfully initialized.
+  allowed = true;
 });
 
+
+window.addEventListener('onEventReceived', function (obj) {
+  if (obj.detail.listener !== 'message') return;
+  
+  if (!allowed) {
+    log("Widget is currently blocked.");
+    return;
+  }
+  
+  if (MediaCommand.isOnGlobalCooldown()) {
+    log("Global cooldown is still running.");
+    return;
+  }
+  
+  /* Since string comparison has the potential to be computationally expensive, 
+   * it is only executed for users with an sufficient permission level. */
+  
+  let data = obj.detail.event.data;
+  let user = data.nick.toLowerCase();
+  
+  // Blocked users will always fail.
+  if (fieldData.blockedUsers.includes(user)) {
+    log(`'${user}' is on blocked users list.`);
+    return;
+  }
+  
+  let userState = {
+    'mod': parseInt(data.tags.mod), 
+    'sub': parseInt(data.tags.subscriber), 
+    'vip': data.tags.badges.includes('vip'), 
+    'broadcaster': (user === channelName)
+  };
+  
+  if ((fieldData.managePermissions === 'everyone') || 
+      (userState.mod && (fieldData.managePermissions === 'mods')) || 
+      ((userState.vip || userState.mod) && (fieldData.managePermissions === 'vips')) || 
+      userState.broadcaster || 
+      fieldData.otherUsers.includes(user)) {
+    
+    log(`'${user}' has sufficient permission level.`);
+    
+    let msg = data.text;
+    
+    /* The string comparision has to happen prior to the evaluation of the
+     * individual cooldown state, because otherwise the whole purpose of a cooldown
+     * is undermined. (If two commands have the same trigger phrase and the first
+     * one is on cooldown, the second one would be choosen instead.) */
+    let mediaCmd = mediaCommands.find((cmd) => cmd.isTriggeredBy(msg));
+    
+    if (!(mediaCmd instanceof MediaCommand)) {
+      log ("No MediaCommand (with an associated URL) declared a match.");
+      return;
+    }
+    
+    if (mediaCmd.isOnCooldown()) {
+      log(`'${mediaCmd.commandStr}' is still on cooldown.`);
+      return;
+    }
+    
+    log(`'${mediaCmd.commandStr}' is executed.`);
+    
+    mediaCmd.play();
+  }
+});
